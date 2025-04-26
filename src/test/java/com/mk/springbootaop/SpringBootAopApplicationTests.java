651d11046ef998d8f1b308bafc6b419f0329b91a
mk-tdev <mk-dev@outlook.com>
2025-04-26 22:52:28 +0800

    project-init

---
+++ src/test/java/com/mk/springbootaop/SpringBootAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mk.springbootaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
